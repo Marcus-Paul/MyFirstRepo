258dbdc3d55ab4f66ae6eedfcd2d9e768abcc28a
paul <paulmarcus135@gmail.com>
2026-07-15 21:57:00 +0530

    sum method added

--- Employee.java
+++ Employee.java
@@ -2,4 +2,11 @@ class Employee{
 	public static void main(String args[]){
 		System.out.println("Hello...");
 	}
+
+	public void sum(){
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+	}
+
 }
