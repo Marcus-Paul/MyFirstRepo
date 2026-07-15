f0c87f05cefc1288344c82cdf122cad41bbcdb90
paul <paulmarcus135@gmail.com>
2026-07-15 21:59:01 +0530

    sub method added

--- Employee.java
+++ Employee.java
@@ -2,4 +2,11 @@ class Employee{
 	public static void main(String args[]){
 		System.out.println("Hello...");
 	}
+
+	public void sub(){
+		int a = 50;
+		int b = 25;
+		System.out.println(a-b);
+	}
+
 }
